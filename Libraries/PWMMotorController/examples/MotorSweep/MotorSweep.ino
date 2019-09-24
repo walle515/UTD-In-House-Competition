6001972d7b64d9a8e28d187dd27b6037e3b8bab0
walle515 <55765933+walle515@users.noreply.github.com>
2019-09-24 15:32:01 -0500

    Add files via upload

---
+++ Libraries/PWMMotorController/examples/MotorSweep/MotorSweep.ino
@@ -0,0 +1,39 @@
+/*  MotorSweep Example
+ *   Written By: Joey Hines (joey.ahines@gmail.com)
+ *   
+ *   Sweeps through all the possible speeds for the motor, 
+ *   from full reverse to full forward.
+ */
+
+//Include PWM_MotorController library
+#include <PWM_MotorController.h>
+
+//Create a PWM_MotorController called motor on pin 3,4, and 5
+// Pin 3: PWM Pin
+// Pin 4: Enable 1
+// Pin 5: Enable 2
+PWM_MotorController motor = PWM_MotorController(3,5,4);
+  
+void setup() {
+  //Begin serial to report speed values to the user
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Loop from -100 to 100, all the possible speeds of the motor
+  for (int i = 0; i <= 100; i++) {
+    //Write i to the motor
+    motor.writeToMotor(i);
+    Serial.println(i);
+
+    //Wait 2 seconds
+    delay(100);
+  }
+
+  //Stop the motor
+  motor.writeToMotor(0);
+
+  //Wait two seconds before begining the next sweep
+  delay(2000);
+
+}
---
+++ Libraries/UTDRWirelessComms/Examples/ParseSerialPacket/ParseSerialPacket.ino
@@ -0,0 +1,45 @@
+/*  ParseSerialPacketExample
+ *  Written By: Joey Hines (joey.ahines@gmail.com)
+ *  
+ *  Parses a packet from serial and prints its part to the user
+ */
+
+#include <UTDRWirelessComms.h>
+#include <SoftwareSerial.h>
+//New SoftwareSerial object for UTDRWirelessComms
+int rx = 8;
+int tx = 7;
+SoftwareSerial serial(rx,tx);
+//Create new UTDRWirelessComms named input
+UTDRWirelessComms input = UTDRWirelessComms(&serial);
+ 
+void setup() {
+  //Setup Serial
+  Serial.begin(9600);
+  Serial.println("Starting...");
+}
+
+void loop() {
+  //Wait for serial
+  if (serial.available()) {
+    //New int array for storing packet data
+    int * packet = new int[3];
+
+    //Get the command packet
+    input.getCommandPacket(packet);
+
+    //Echo packet to user
+    Serial.println("Packet: ");
+    for (int i = 0; i < 3; i++) {
+      Serial.print(packet[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+
+    //Clear packet memory
+    delete packet;
+  }
+
+  delay(20);
+
+}
